fdb63982f08bab43d235b1eb049dc959170a237c
Ronald White <ron.white@trackvia.com>
2011-02-08 09:36:46 -0700

    add auto log done for org

--- rwhite.el
+++ rwhite.el
@@ -1,2 +1,7 @@
 (setq tramp-default-method "ssh")
 (color-theme-blackboard)
+(require 'org-install)
+(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
