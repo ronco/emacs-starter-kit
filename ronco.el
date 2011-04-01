d71d4e358b0b9bd3978f60d52c25672660cd3957
Ronald White <ron.white@trackvia.com>
2011-04-01 13:45:41 -0600

    Added nxhtml

--- ronco.el
+++ ronco.el
@@ -5,7 +5,15 @@
 (if window-system
     (color-theme-blackboard))
 
+;; behavioral stuff
+
+(setq ns-pop-up-frames 'nil)
+
 ;; tabs
+(defun 4x4-spaces ()
+  "Setting to use spaces for tabs at a width of 4"
+  (setq indent-tabs-mode nil)
+  (setq tab-width 4))
 (defun 4x4-tabs ()
   "Setting to use tabs at a width of 4"
   (setq indent-tabs-mode t)
@@ -18,7 +26,7 @@
   (interactive)
   (if (use-region-p)
       (save-excursion
-	(shell-command-on-region (point) (mark) "perltidy -t -nola -q" nil t))
+	(shell-command-on-region (point) (mark) "perltidy -l=90 -nola -q" nil t))
       (save-excursion (mark-whole-buffer) (perltidy-region))
     )
   )
@@ -30,8 +38,11 @@
 
 (defun ron-perl-hook ()
   "My perl settings"
-  (4x4-tabs)
-  (setq cperl-indent-level 4)
+  (cperl-set-style "BSD")
+  (4x4-spaces)
+  (setq cperl-indent-level 4
+        cperl-indent-parens-as-block t
+        cperl-close-paren-offset -4)
   (define-key cperl-mode-map [f5] 'perltidy-defun)
   (define-key cperl-mode-map [f6] 'perltidy-region)
   )
@@ -39,9 +50,31 @@
 
 (add-hook 'cperl-mode-hook 'ron-perl-hook)
 
+;; TT stuff
+(require 'mumamo-fun)
+(setq auto-mode-alist (append '(("\\.tt$" . tt-html-mumamo)) auto-mode-alist))
+(defun ron-tt-html-mumamo-hook
+  (local-set-key (kbd "M-TAB") 'nxml-complete)
+  )
+
+(add-hook 'tt-html-mumamo-mode 'ron-tt-html-mumamo-hook)
+
+;; Obj-c stuff
+(defun ron-objc-hook ()
+  "My Objective-C settings"
+  (c-set-style "BSD")
+  (4x4-tabs)
+  (local-set-key (kbd "RET") 'newline-and-indent)
+  )
+
+(add-hook 'objc-mode-hook 'ron-objc-hook)
+
 ;; lisp stuff
 
 
+;; Modes
+(when (fboundp 'winner-mode)
+      (winner-mode 1))
 
 ;; GLOBAL BINDINGS
 
@@ -51,22 +84,24 @@
 (fset 'twin-screen
    "\C-x1\C-x3\C-x+")
 (global-set-key "\M-2"     'twin-screen)
+(fset 'triple-u-screen
+      "\C-x1\C-x2\C-x3\C-u15\C-x^")
+(global-set-key "\M-4"     'triple-u-screen)
 (global-set-key "\C-c/"     'comment-or-uncomment-region)
 
 ;; org
-(setq org-enforce-todo-dependencies t)
-(setq org-enforce-todo-checkbox-dependencies t)
 (setq org-clock-idle-time 5)
 (require 'org-install)
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 (setq org-log-done t)
 (setq org-mobile-directory "~/Dropbox/MobileOrg")
-(setq org-directory "~/org")
+(setq org-directory "~/Dropbox/org")
 (setq org-default-notes-file (concat org-directory "/notes.org"))
 (setq org-agenda-files (list (concat org-directory "/work.org")
                              (concat org-directory "/home.org")
-                             (concat org-directory "/auth_release.org")
+                             (concat org-directory "/someday.org")
                              org-default-notes-file))
+(setq org-refile-targets (quote ((org-agenda-files :maxlevel . 2))))
 
 (setq org-todo-keywords
       '((sequence "TODO" "|" "DONE")
